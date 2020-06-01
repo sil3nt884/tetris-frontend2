deee790ef43d2abd080f92ac8c566b83beaa8b1e
ricky <sil3nt884@hotmail.co.uk>
2020-06-02 00:29:51 +0100

    Added BaseURL to config object

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,12 @@ const evn = global.environment;
 const getConfig = require('./../config');
 
 const menu = (req, res) => {
-  const config = JSON.stringify({ backendPort: getConfig('app.backendPort'), mulitiPlayerTimeout: getConfig('app.mulitiPlayerTimeout' ) });
+  const config = JSON.stringify(
+      {
+        backendPort: getConfig('app.backendPort'),
+        mulitiPlayerTimeout: getConfig('app.mulitiPlayerTimeout'),
+        baseURL: getConfig('app.baseURL'),
+      });
 
   if (evn === 'prod') {
     res.render('index', { config: config, env: 'bundle.js' });
